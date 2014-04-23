8cb4a74848e40fad3c68b2a082048fd082ee4e39
Eric Kansa <ekansa@alexandriaarchive.org>
2014-04-22 18:02:46 -0700

    1st iteration project structure
    Managed to successfully interact with the database!

---
+++ opencontext_py/apps/manifest/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Manifest(models.Model):
+    uuid = models.CharField(max_length=50, primary_key=True)
+    label = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'oc_manifest'
+    def __str__(self):
+        return self.label
