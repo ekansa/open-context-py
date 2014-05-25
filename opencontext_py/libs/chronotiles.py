b6638492738d325b76ad65a93de1141c025e83cd
Eric Kansa <kansaeric@gmail.com>
2014-05-24 17:32:20 -0700

    More chrono tile work

--- opencontext_py/libs/chronotiles.py
+++ opencontext_py/libs/chronotiles.py
@@ -13,29 +13,64 @@ class ChronoTile():
 
     """
 
-    DEFAULT_MAXIMUM_BP = 10000000  # 10 million years ago
-    MAX_TILE_DEPTH = 30
-    MIN_INTERVAL_SPAN = 1
-    PREFIX_DELIM = '-'
-    block_start = False
-    block_end = False
-    path_max_bp = False
-
     def __init__(self):
-        DEFAULT_MAXIMUM_BP = 10000000  # 10 million years ago
-        MAX_TILE_DEPTH = 30
-        MIN_INTERVAL_SPAN = 1
-        PREFIX_DELIM = '-'
-        block_start = False
-        block_end = False
-        path_max_bp = False
+        self.DEFAULT_MAXIMUM_BP = 10000000  # 10 million years ago
+        self.MAX_TILE_DEPTH = 30
+        self.MIN_INTERVAL_SPAN = 1
+        self.PREFIX_DELIM = '-'
+        self.block_latest = 0
+        self.block_earliest = DEFAULT_MAXIMUM_BP
+        self.path_max_bp = DEFAULT_MAXIMUM_BP
+
+    def decode_path(self, raw_path):
+        """
+        decodes a path to return start and end dates
+        """
+        self.get_path_maximum(raw_path)
+        if(self.PREFIX_DELIM in raw_path):
+            path_ex = raw_path.split(self.PREFIX_DELIM)
+            path = path_ex[1]
+        else:
+            path = raw_path
+        t_path = re.sub('[^0-3]', '', path)
+        if(t_path != path):
+            # path contains invalid characters, return False
+            return False
+        else:
+            return self.get_path_interval(path)
+    
+    def get_path_interval(self, path):
+        """
+        converts a path to a time interval
+        """
+        path_depth = len(path)
+        self.block_latest = 0
+        self.block_eariest = self.path_max_bp
+        level_interval = self.path_max_bp
+        i = 0
+        while(i < path_depth):
+            level_interval = level_interval / 2
+            act_path_square = path[i]
+            if(act_path_square == '0'):
+                self.block_eariest -= level_interval
+            elif(act_path_square == '1'):
+                # nothing happens
+            
+            elif(act_path_square == '2'):
+                self.block_latest += level_interval
+                self.block_earliest -= level_interval
+            elif(act_path_square == '3'):
+                self.block_latest += level_interval
+            else:
+                # wildy wrong character
+                return False
+            i += 1
+        return level_interval
 
     def get_path_maximum(self, raw_path):
         """
         Reads a path and checks for a prefix to indicate what the maximum value can be
         """
-
-        self.path_max_bp = self.DEFAULT_MAXIMUM_BP
         exp_dict = {'k': 3,
                     'm': 6,
                     'g': 9}
