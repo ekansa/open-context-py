2880edb1766fac9bc5d6329b971acd7143638edb
Eric Kansa <ekansa@alexandriaarchive.org>
2014-06-02 11:55:33 -0700

    Fixed problem with unique key on uuid
    Need to allow UUID to occur multiple times

--- opencontext_py/apps/ocitems/mediafiles/models.py
+++ opencontext_py/apps/ocitems/mediafiles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Mediafile has basic metadata about media resources (binary files) associated with a media resource item
 class Mediafile(models.Model):
-    uuid = models.CharField(max_length=50, primary_key=True)
+    uuid = models.CharField(max_length=50, db_index=True)
     project_uuid = models.CharField(max_length=50, db_index=True)
     source_id = models.CharField(max_length=50, db_index=True)
     file_type = models.CharField(max_length=50, db_index=True)
