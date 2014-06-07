cd3d1611be943041b0f7920a869b279def9f1259
Eric Kansa <ekansa@alexandriaarchive.org>
2014-06-07 00:01:04 -0700

    Will be deprecated
    Function to move data before deleting table and app

--- opencontext_py/apps/ldata/linkhierarchies/models.py
+++ opencontext_py/apps/ldata/linkhierarchies/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from opencontext_py.apps.ldata.linkannotations.models import LinkAnnotation, LinkRecursion
+from opencontext_py.apps.entities.uri.models import URImanagement
 
 
 # This class stores linked data hierarchies, useful for facetet search expansion
@@ -12,3 +14,18 @@ class LinkHierarchy(models.Model):
 
     class Meta:
         db_table = 'link_hierarchies'
+
+
+class MigrateHierarchy():
+    def migrate(self):
+        lh_list = LinkHierarchy.objects.all()
+        for lh in lh_list:
+            new_la = LinkAnnotation()
+            new_la.subject = lh.child_uri
+            new_la.subject_type = 'uri'
+            new_la.project_uuid = '0'
+            new_la.source_id = 'LinkHierarchy'
+            new_la.predicate_uri = 'skos:broader'
+            new_la.object_uri = lh.parent_uri
+            new_la.creator_uuid = ''
+            new_la.save()
