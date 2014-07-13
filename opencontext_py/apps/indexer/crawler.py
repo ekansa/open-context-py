f482ea6744beaac28818216fbcdfeb338794aa64
John Ward <johnbward@gmail.com>
2014-07-13 11:21:29 -0700

    Adds improved error checking and single document indexing

--- opencontext_py/apps/indexer/crawler.py
+++ opencontext_py/apps/indexer/crawler.py
@@ -13,15 +13,63 @@ class Crawler():
         self.solr = Solr('http://localhost:8983/solr',
                          make_request=self.session)
 
-    def crawl(self, chunksize=100):
+    def crawl(self, chunksize=500):
+        document_count = 0
         while self.uuidlist is not None:
             documents = []
-            print('creating container of ' + str(chunksize) + ' documents...')
+            print('Creating container of ' + str(chunksize) + ' documents...')
             for uuid in islice(self.uuidlist, 0, chunksize):
                 try:
-                    solrdocument = SolrDocument(uuid)
-                    documents.append(solrdocument.fields)
-                    print('adding ' + uuid)
+                    solrdocument = SolrDocument(uuid).fields
+                    if self.document_is_valid(solrdocument):
+                        documents.append(solrdocument)
+                        document_count += 1
+                        print('(' + str(document_count) + ') adding ' + uuid)
+                    else:
+                        print('Error: Datatype mismatch -----> ' + uuid)
                 except Exception as err:
-                    print("Error: {0}".format(err) + " ---> " + uuid)
+                    print("Error: {0}".format(err) + " -----> " + uuid)
             self.solr.update(documents, 'json', commit=True)
+            if self.solr.update(documents, 'json', commit=True).status != 200:
+                print('Error: ' + str(self.solr.update(
+                    documents, 'json', commit=True
+                    ).raw_content))
+
+    def index_single_document(self, uuid):
+        documents = []
+        try:
+            solrdocument = SolrDocument(uuid).fields
+            if self.document_is_valid(solrdocument):
+                documents.append(solrdocument)
+                self.solr.update(documents, 'json', commit=True)
+                if self.solr.update(
+                        documents,
+                        'json',
+                        commit=True
+                        ).status != 200:
+                    print('Error: ' + str(self.solr.update(
+                        documents, 'json', commit=True
+                        ).raw_content))
+                else:
+                    print('Successfully indexed ' + uuid)
+            else:
+                print('Error: Datatype mismatch -----> ' + uuid)
+        except Exception as err:
+            print("Error: {0}".format(err) + " -----> " + uuid)
+
+    def document_is_valid(self, document):
+        is_valid = True
+        for key in document:
+            if key.endswith('numeric'):
+                if not(self.valid_float(document[key])):
+                    is_valid = False
+        return is_valid
+
+    def valid_float(self, value):
+        # The values we are checking could be either (mulit-valued) lists
+        # or individual values. We need to check both cases.
+        # Check if all items in a list are floats
+        if isinstance(value, list):
+            return all(isinstance(item, float) for item in value)
+        else:
+            return isinstance(value, float)
--- opencontext_py/apps/indexer/solrdocument.py
+++ opencontext_py/apps/indexer/solrdocument.py
@@ -5,12 +5,16 @@ from opencontext_py.apps.ldata.linkannotations.models import LinkRecursion
 
 
 class SolrDocument:
-    ''' Defines the Solr Document objects that the crawler will crawl. Solr
-    fields are stored in the Solr Documents' "fields" property. '''
+    '''
+    Defines the Solr Document objects that the crawler will crawl. Solr
+    fields are stored in the Solr Documents' "fields" property.
+    '''
 
     def __init__(self, uuid):
-        ''' Using our Python JSON-LD and other info provided in OCitem,
-        build up dictionary of solr fields to index.'''
+        '''
+        Using our Python JSON-LD and other info provided in OCitem,
+        build up dictionary of solr fields to index.
+        '''
         # get ocitem, from which we can also access json_ld
         self.oc_item = OCitem().get_item(uuid)
         self.context_path = self._get_context_path()
@@ -19,15 +23,55 @@ class SolrDocument:
         self._process_context_path()
         self._process_predicates()
 
+    def _get_solr_field_name_suffix(self, predicate_type):
+        '''
+        Defines whether our dynamic solr fields names for
+        predicates end with ___pred_id, ___pred_numeric, etc.
+        '''
+        if predicate_type == '@id':
+            return '___pred_id'
+        elif predicate_type in ['xsd:integer', 'xsd:double', 'xsd:boolean']:
+            return '___pred_numeric'
+        elif predicate_type == 'xsd:string':
+            return '___pred_string'
+        elif predicate_type == 'xsd:date':
+            return '___pred_date'
+        else:
+            raise Exception("Error: Unknown predicate type")
+
+    def _get_predicate_values(self, predicate_slug, predicate_type):
+        obs_key = 'oc-pred:' + predicate_slug
+        for obs_list in self.oc_item.json_ld['oc-gen:has-obs']:
+            if obs_key in obs_list:
+                if predicate_type == '@id':
+                    return self._convert_values_to_json(
+                        obs_list[obs_key][0]['slug'],
+                        obs_list[obs_key][0]['label']
+                        )
+                elif predicate_type in [
+                    'xsd:integer', 'xsd:double', 'xsd:boolean'
+                        ]:
+                    if len(obs_list[obs_key]) == 1:
+                        return obs_list[obs_key][0]
+                    else:
+                        return obs_list[obs_key]
+                elif predicate_type == 'xsd:date':
+                    return obs_list[obs_key][0] + 'T00:00:00Z'
+                elif predicate_type == 'xsd:string':
+                    return obs_list[obs_key][0]['xsd:string']
+                else:
+                    raise Exception("Error: Could not get predicate value")
+
     def _process_predicates(self):
         # Get list of predicates
         predicates = (item for item in self.oc_item.json_ld[
             '@context'].items() if item[0].startswith('oc-pred:'))
-        # We need a list for "root___pred_id" because it is multivalued
+        # We need a list for "root___pred_id" because it is multi-valued
         self.fields['root___pred_id'] = []
         for predicate in predicates:
             # We need the predicate's uuid to get its parents
             predicate_uuid = predicate[1]['owl:sameAs'].split('/')[-1]
+            predicate_type = predicate[1]['@type']
             parents = LinkRecursion(
                 ).get_jsonldish_entity_parents(predicate_uuid)
             # Process parents
@@ -40,22 +84,47 @@ class SolrDocument:
                             parent['label']
                             )
                         )
-                    # if it's also the last item (i.e., no parents)
                     if len(parents) == 1:
-                        # Then process the predicate values
-                        # get the solr field name suffix
-                        pass
+                    # if it's the only item (i.e., no parents),
+                        # process the predicate values
+                        # First generate the solr field name
+                        solr_field_name = self._convert_slug_to_solr(
+                            parent['slug'] +
+                            self._get_solr_field_name_suffix(
+                                predicate_type)
+                            )
+                        # Then get the predicate values
+                        self.fields[solr_field_name] = \
+                            self._get_predicate_values(
+                                parent['slug'],
+                                predicate_type
+                            )
                 else:
                     # Process additional items
                     # Create solr field name using parent slug
-                    # Add slug and label as json
+                    solr_field_name = \
+                        parents[index - 1]['slug'] + '___pred_id'
+                    solr_field_name = self._convert_slug_to_solr(
+                        solr_field_name
+                        )
+                    # Add slug and label as json values
+                    self.fields[solr_field_name] = \
+                        self._convert_values_to_json(
+                            parent['slug'],
+                            parent['label']
+                            )
                     # If this is the last item, process the predicate values
                     if index == len(parents) - 1:
-                        pass
-                    # First get the slug of the parent for the solr field name
-                    # Get the solr field name suffix based on the type
-                    # (int, sting, date, etc.)
-                    # Get the predicate value using an appropriate  method
+                        solr_field_name = self._convert_slug_to_solr(
+                            parent['slug'] +
+                            self._get_solr_field_name_suffix(
+                                predicate_type)
+                            )
+                        self.fields[solr_field_name] = \
+                            self._get_predicate_values(
+                                parent['slug'],
+                                predicate_type
+                            )
 
     def _get_context_path(self):
         if 'oc-gen:has-context-path' in self.oc_item.json_ld:
@@ -109,14 +178,14 @@ class SolrDocument:
                 else:
                 # for others, get the parent slug and generate a
                 # dynamic field name
-                    self._solr_field_name = \
+                    solr_field_name = \
                         self.context_path[index - 1]['slug'] + '___context_id'
                     # replace dashes with underscores because solr requires it
-                    self._solr_field_name = self._convert_slug_to_solr(
-                        self._solr_field_name
+                    solr_field_name = self._convert_slug_to_solr(
+                        solr_field_name
                         )
                     # add field name and values as json
-                    self.fields[self._solr_field_name] = \
+                    self.fields[solr_field_name] = \
                         self._convert_values_to_json(
                             self.context_path[index]['slug'],
                             self.context_path[index]['label']
