0360fdb42ea60bf5b217b4e16ec7d41cf35a6d3c
John Ward <johnbward@gmail.com>
2014-07-13 23:30:56 -0700

    Small style changes and naming improvements

--- opencontext_py/apps/indexer/crawler.py
+++ opencontext_py/apps/indexer/crawler.py
@@ -21,7 +21,7 @@ class Crawler():
             for uuid in islice(self.uuidlist, 0, chunksize):
                 try:
                     solrdocument = SolrDocument(uuid).fields
-                    if self.document_is_valid(solrdocument):
+                    if self._document_is_valid(solrdocument):
                         documents.append(solrdocument)
                         document_count += 1
                         print('(' + str(document_count) + ') adding ' + uuid)
@@ -39,17 +39,15 @@ class Crawler():
         documents = []
         try:
             solrdocument = SolrDocument(uuid).fields
-            if self.document_is_valid(solrdocument):
+            if self._document_is_valid(solrdocument):
                 documents.append(solrdocument)
                 self.solr.update(documents, 'json', commit=True)
                 if self.solr.update(
-                        documents,
-                        'json',
-                        commit=True
-                        ).status != 200:
-                    print('Error: ' + str(self.solr.update(
-                        documents, 'json', commit=True
-                        ).raw_content))
+                        documents, 'json', commit=True).status != 200:
+                        print('Error: ' + str(self.solr.update(
+                            documents, 'json', commit=True
+                            ).raw_content)
+                            )
                 else:
                     print('Successfully indexed ' + uuid)
             else:
@@ -57,15 +55,15 @@ class Crawler():
         except Exception as err:
             print("Error: {0}".format(err) + " -----> " + uuid)
 
-    def document_is_valid(self, document):
+    def _document_is_valid(self, document):
         is_valid = True
         for key in document:
             if key.endswith('numeric'):
-                if not(self.valid_float(document[key])):
+                if not(self._valid_float(document[key])):
                     is_valid = False
         return is_valid
 
-    def valid_float(self, value):
+    def _valid_float(self, value):
         # The values we are checking could be either (mulit-valued) lists
         # or individual values. We need to check both cases.
         # Check if all items in a list are floats
