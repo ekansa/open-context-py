2a48125697c30febeac76c247e8780a776d3d121
John Ward <johnbward@gmail.com>
2014-07-14 22:53:01 -0700

    adds documents per second and improves single document crawling.

--- opencontext_py/apps/indexer/crawler.py
+++ opencontext_py/apps/indexer/crawler.py
@@ -1,3 +1,5 @@
+import time
+import re
 from itertools import islice
 import requests
 from mysolr import Solr
@@ -13,29 +15,36 @@ class Crawler():
         self.solr = Solr('http://localhost:8983/solr',
                          make_request=self.session)
 
-    def crawl(self, chunksize=500):
+    def crawl(self, chunksize=100):
+        start_time = time.time()
+        print('\nStarting crawl...\n')
+        print('(#) UUID')
+        print('----------------------------------------')
         document_count = 0
         while self.uuidlist is not None:
             documents = []
-            print('Creating container of ' + str(chunksize) + ' documents...')
             for uuid in islice(self.uuidlist, 0, chunksize):
                 try:
                     solrdocument = SolrDocument(uuid).fields
                     if self._document_is_valid(solrdocument):
                         documents.append(solrdocument)
                         document_count += 1
-                        print('(' + str(document_count) + ') adding ' + uuid)
+                        print('(' + str(document_count) + ') ' + uuid)
                     else:
-                        print('Error: Datatype mismatch -----> ' + uuid)
+                        print('Error: Skipping document due to datatype '
+                              'mismatch -----> ' + uuid)
                 except Exception as err:
                     print("Error: {0}".format(err) + " -----> " + uuid)
             self.solr.update(documents, 'json', commit=True)
+            print('Crawl Rate: ' + self._documents_per_second(
+                start_time, document_count) + ' documents per second')
             if self.solr.update(documents, 'json', commit=True).status != 200:
                 print('Error: ' + str(self.solr.update(
                     documents, 'json', commit=True
                     ).raw_content))
 
     def index_single_document(self, uuid):
+        print('\nAttempting to index document ' + uuid + '...\n')
         documents = []
         try:
             solrdocument = SolrDocument(uuid).fields
@@ -49,9 +58,10 @@ class Crawler():
                             ).raw_content)
                             )
                 else:
-                    print('Successfully indexed ' + uuid)
+                    print('Successfully indexed ' + uuid + '.')
             else:
-                print('Error: Datatype mismatch -----> ' + uuid)
+                print('Error: Unable to index ' + uuid + ' due to '
+                      'datatype mismatch.')
         except Exception as err:
             print("Error: {0}".format(err) + " -----> " + uuid)
 
@@ -61,13 +71,25 @@ class Crawler():
             if key.endswith('numeric'):
                 if not(self._valid_float(document[key])):
                     is_valid = False
+            if key.endswith('date'):
+                if not(self._valid_date(document[key])):
+                    is_valid = False
         return is_valid
 
     def _valid_float(self, value):
-        # The values we are checking could be either (mulit-valued) lists
-        # or individual values. We need to check both cases.
-        # Check if all items in a list are floats
-        if isinstance(value, list):
+        if isinstance(value, float):
+            return True
+        elif isinstance(value, list):
+            # If it's a list, make sure all items are floats
             return all(isinstance(item, float) for item in value)
         else:
-            return isinstance(value, float)
+            return False
+
+    def _valid_date(self, value):
+        pattern = re.compile(
+            '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?Z'
+            )
+        return bool(pattern.search(value))
+
+    def _documents_per_second(self, start_time, document_count):
+        return str(int(document_count//(time.time() - start_time)))
