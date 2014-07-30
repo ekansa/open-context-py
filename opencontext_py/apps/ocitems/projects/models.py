8ee5ba44f88bb3ed5fa5f8a347110173613919aa
Eric Kansa <ekansa@alexandriaarchive.org>
2014-07-29 20:38:16 -0700

    Begin work on project geo/chrono metadata
    Will summarize data associated with the project to create metadata.

--- opencontext_py/apps/ocitems/projects/models.py
+++ opencontext_py/apps/ocitems/projects/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models import Avg, Max, Min
+from opencontext_py.apps.ocitems.geospace.models import Geospace
+from opencontext_py.apps.ocitems.events.models import Event
+from opencontext_py.libs.chronotiles import ChronoTile
+from opencontext_py.libs.globalmaptiles import GlobalMercator
 
 
 # Project stores the content of a project resource (structured text)
@@ -35,3 +40,37 @@ class ProjectRels():
         else:
             self.sub_projects = False
         return self.sub_projects
+
+
+class ProjectMeta():
+    """
+    Generates geospatial and chronological summaries for project metadata
+    """
+
+    def __init__(self):
+        self.geo_ents = False
+        self.event_ents = False
+        self.geo_range = False
+
+    def get_geo_range(self, uuids):
+        """
+        Gets summary geospatial information for a string or a list
+        of project uuids. Accepts a list to get sub-projects
+        """
+        if uuids is not list:
+            uuids = [uuids]  # make a list
+        proj_geo = Geospace.objects.filter(project_uuid__in=uuids)\
+                           .exclude(latitude=0, longitude=0)\
+                           .aggregate(Max('latitude'),
+                                      Max('longitude'),
+                                      Min('latitude'),
+                                      Min('longitude'))
+        if(proj_geo['latitude__max'] is not None):
+            """
+            self.max_lat = proj_geo['latitude__max']
+            self.min_lat = proj_geo['latitude__min']
+            self.max_lon = proj_geo['longitude__max']
+            self.min_lon = proj_geo['longitude__min']
+            """
+            self.geo_range = proj_geo
+
