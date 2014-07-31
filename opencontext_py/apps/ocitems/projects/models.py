ee35d5e7470f218d4fe4799da3f263de95597d8b
Eric Kansa <kansaeric@gmail.com>
2014-07-31 13:28:28 -0700

    Working kmeans clustering for proj geo data
    Needs additional work to combine clusters that are too small, but at
    least this gets NumPy and SciPy to work

--- opencontext_py/apps/ocitems/projects/models.py
+++ opencontext_py/apps/ocitems/projects/models.py
@@ -1,3 +1,6 @@
+import numpy as np
+from numpy import vstack, array
+from scipy.cluster.vq import kmeans,vq
 from django.db import models
 from django.db.models import Avg, Max, Min
 from opencontext_py.apps.ocitems.geospace.models import Geospace
@@ -47,18 +50,55 @@ class ProjectMeta():
     Generates geospatial and chronological summaries for project metadata
     """
 
+    MAX_CLUSTERS = 15
+    MIN_CLUSTER_SIZE = .05
+
     def __init__(self):
         self.geo_ents = False
         self.event_ents = False
         self.geo_range = False
+
+    def make_uuids_list(self, uuids):
+        """ Makes a list of UUIDS if only passed a string """
+        if uuids is not list:
+            uuids = [uuids]  # make a list
+        return uuids
+
+    def cluster_geo(self, uuids):
+        """ Puts geo points into clusters """
+        dist_geo = self.get_distinct_geo(uuids)
+        lon_lats = []
+        for geo in dist_geo:
+            # using geojson order of lon / lat
+            lon_lat = [geo['longitude'], geo['latitude']]
+            # makes it a numpy float
+            dpoint = np.fromiter(lon_lat, np.dtype('float'))
+            lon_lats.append(dpoint)
+        # create a numpy array object from my list of float coordinates
+        data = array(lon_lats)
+        resonable_clusters = False
+        number_clusters = self.MAX_CLUSTERS
+        while resonable_clusters is False:
+            centroids, _ = kmeans(data, number_clusters)
+            idx, _ = vq(data, centroids)
+            resonable_clusters = True
+        return centroids
+
+    def get_distinct_geo(self, uuids):
+        """ Gets distinct geo lat/lons """
+        uuids = self.make_uuids_list(uuids)
+        dist_geo = Geospace.objects.filter(project_uuid__in=uuids)\
+                           .exclude(latitude=0, longitude=0)\
+                           .values('latitude', 'longitude')\
+                           .distinct()
+        return dist_geo
 
     def get_geo_range(self, uuids):
         """
         Gets summary geospatial information for a string or a list
         of project uuids. Accepts a list to get sub-projects
         """
-        if uuids is not list:
-            uuids = [uuids]  # make a list
+        uuids = self.make_uuids_list(uuids)
         proj_geo = Geospace.objects.filter(project_uuid__in=uuids)\
                            .exclude(latitude=0, longitude=0)\
                            .aggregate(Max('latitude'),
