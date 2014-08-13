c6165cde85b19fc3a478718cc856556169f91895
Eric Kansa <ekansa@alexandriaarchive.org>
2014-08-12 18:38:48 -0700

    Get uuid list from export table

--- opencontext_py/apps/exports/records/uuidlist.py
+++ opencontext_py/apps/exports/records/uuidlist.py
@@ -1,14 +1,28 @@
 from django.db.models import F
 from django.db.models import Q
 from opencontext_py.apps.ocitems.manifest.models import Manifest
+from opencontext_py.apps.exports.records.models import ExpCell
 
 
 class UUIDListSimple:
     '''
-    The list of UUIDs that the crawler will crawl.
+    The list of UUIDs used to create an export table.
     '''
     def __init__(self, project_uuids, class_uri):
         self.uuids = Manifest.objects.values_list(
             'uuid', flat=True)\
             .filter(project_uuid__in=project_uuids,
                     class_uri=class_uri).iterator()
+
+
+class UUIDListExportTable:
+    '''
+    The list of UUIDs in an export table.
+    '''
+    def __init__(self, table_id):
+        self.uuids = ExpCell.objects\
+                            .values_list('uuid', flat=True)\
+                            .order_by()\
+                            .filter(table_id=table_id)\
+                            .distinct()\
+                            .iterator()
