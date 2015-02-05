dc12ac6b2158a6213915a766afbb056e9be23ee0
Eric Kansa <kansaeric@gmail.com>
2015-02-05 07:34:13 -0800

    Start DINAA trinomial edits
    This is not really part of the whole project, but stuck in here to
    document process.

---
+++ opencontext_py/apps/edit/dinaa/trinomials/models.py
@@ -0,0 +1,20 @@
+import hashlib
+from django.db import models
+
+
+# A model for Smithsonian Trinomials specific to the DINAA project
+# This model is used strictly for DINAA data processing, and is
+# not meant to be a permananent part of the Open Context project
+class Trinomial(models.Model):
+
+    uri = models.CharField(max_length=200, db_index=True)
+    uuid = models.CharField(max_length=50, primary_key=True)
+    label = models.CharField(max_length=200, db_index=True, null=True)
+    project_label = models.CharField(max_length=200)
+    trinomial = models.CharField(max_length=50, db_index=True, null=True)
+    state = models.CharField(max_length=4, null=True)
+    county = models.CharField(max_length=4, null=True)
+    site = models.CharField(max_length=8, null=True)
+
+    class Meta:
+        db_table = 'dinaa_trinomials'
