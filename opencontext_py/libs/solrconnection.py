09961b74934f8de4596ffeae4f4f6174f13635a5
Eric Kansa <kansaeric@gmail.com>
2015-02-13 04:30:27 -0800

    Improved Solr problem handling
    So as to return a web page with a 500 error

--- opencontext_py/libs/solrconnection.py
+++ opencontext_py/libs/solrconnection.py
@@ -9,9 +9,12 @@ class SolrConnection():
     Provides a connection to our Solr instance. This is useful for both
     crawling and searching.
     '''
-    def __init__(self, solr_host=settings.SOLR_HOST, solr_port=settings.SOLR_PORT):
+    def __init__(self, exit_on_error=True, solr_host=settings.SOLR_HOST, solr_port=settings.SOLR_PORT):
+        if 'http://' not in solr_host and 'https://' not in solr_host:
+            # forgiving of configurations
+            solr_host = 'http://' + solr_host
         self.session = requests.Session()
-        solr_connection_string = 'http://' + solr_host + ':' + str(solr_port) \
+        solr_connection_string = solr_host + ':' + str(solr_port) \
             + '/solr'
         try:
             self.connection = Solr(solr_connection_string,
@@ -19,4 +22,7 @@ class SolrConnection():
         except requests.ConnectionError:
             print('\nError: Could not connect to Solr. Please '
                   'verify your Solr instance and configuration.\n')
-            sys.exit(1)
+            if exit_on_error:
+                sys.exit(1)
+            else:
+                self.connection = False
