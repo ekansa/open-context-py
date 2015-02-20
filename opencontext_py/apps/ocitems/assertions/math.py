52cd6e37cb33631d2599c3783a7f98e9aa562f19
Eric Kansa <kansaeric@gmail.com>
2015-02-20 06:17:16 -0800

    Bug fix numeric search, hierarchies
    Fixed a big discovered in a live demo (of course) involving numeric
    hierarchy fields.

--- opencontext_py/apps/ocitems/assertions/math.py
+++ opencontext_py/apps/ocitems/assertions/math.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.db.models import Max, Min, Count, Avg
+from opencontext_py.apps.entities.uri.models import URImanagement
 from opencontext_py.apps.ocitems.assertions.models import Assertion
 from opencontext_py.apps.ldata.linkannotations.equivalence import LinkEquivalence
-from opencontext_py.apps.entities.uri.models import URImanagement
+from opencontext_py.apps.ldata.linkannotations.recursion import LinkRecursion
 
 
 class MathAssertions():
@@ -25,7 +26,7 @@ class MathAssertions():
         predicate_uuids = lequiv.get_from_object(object_uri)
         return self.get_date_range(predicate_uuids)
 
-    def get_numeric_range(self, predicate_uuids):
+    def get_numeric_range(self, predicate_uuids, children_ok=True):
         if not isinstance(predicate_uuids, list):
             predicate_uuids = [str(predicate_uuids)]
         sum_ass = Assertion.objects\
@@ -39,6 +40,26 @@ class MathAssertions():
         output['min'] = sum_ass['data_num__min']
         output['max'] = sum_ass['data_num__max']
         output['count'] = sum_ass['hash_id__count']
+        if output['count'] == 0 and children_ok:
+            output = self.get_numeric_range_from_children(predicate_uuids)
+        return output
+
+    def get_numeric_range_from_children(self, predicate_uuids):
+        """ gets numeric range from a list of predicates
+            OR their children
+        """
+        if not isinstance(predicate_uuids, list):
+            predicate_uuids = [str(predicate_uuids)]
+        all_preds = []
+        for predicate_uuid in predicate_uuids:
+            all_preds.append(predicate_uuid)
+            lr = LinkRecursion()
+            lr.get_entity_children(predicate_uuid)
+            for child_uuid_key, val in lr.child_entities.items():
+                if child_uuid_key not in all_preds:
+                    all_preds.append(child_uuid_key)
+        # now get the summary, but don't allow recursive looking at children
+        output = self.get_numeric_range(all_preds, False)
         return output
 
     def get_date_range(self, predicate_uuids):
@@ -54,3 +75,4 @@ class MathAssertions():
         output['max'] = sum_ass['data_date__max']
         output['count'] = sum_ass['hash_id__count']
         return output
+
