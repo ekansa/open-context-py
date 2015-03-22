ce4f426f1f699fd587dfb982e8f24f0f9872d969
Eric Kansa <kansaeric@gmail.com>
2015-03-22 16:40:07 -0700

    Start of 'about' pages
    With a beginning of a recipes page for using the API

--- opencontext_py/apps/about/views.py
+++ opencontext_py/apps/about/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from opencontext_py.libs.general import LastUpdatedOrderedDict
+from opencontext_py.libs.rootpath import RootPath
 from opencontext_py.libs.requestnegotiation import RequestNegotiation
 
 
@@ -10,40 +11,19 @@ def index(request, spatial_context=None):
     return HttpResponse("Hello, world. You're at the About index.")
 
 
-def item_view(request):
-    """ Get the item context JSON-LD """
-    item_context_obj = ItemContext()
-    json_ld = LastUpdatedOrderedDict()
-    json_ld['@context'] = item_context_obj.context
-    req_neg = RequestNegotiation('application/json')
-    req_neg.supported_types = ['application/ld+json']
-    if 'HTTP_ACCEPT' in request.META:
-        req_neg.check_request_support(request.META['HTTP_ACCEPT'])
-    if req_neg.supported:
-        # requester wanted a mimetype we DO support
-        return HttpResponse(json.dumps(json_ld,
-                            ensure_ascii=False, indent=4),
-                            content_type=req_neg.use_response_type + "; charset=utf8")
-    else:
-        # client wanted a mimetype we don't support
-        return HttpResponse(req_neg.error_message,
-                            status=415)
-
-
-def search_view(request):
+def recipes_view(request):
     """ Get the search context JSON-LD """
-    search_context_obj = SearchContext()
-    json_ld = LastUpdatedOrderedDict()
-    json_ld['@context'] = search_context_obj.context
-    req_neg = RequestNegotiation('application/json')
-    req_neg.supported_types = ['application/ld+json']
+    rp = RootPath()
+    base_url = rp.get_baseurl()
+    req_neg = RequestNegotiation('text/html')
     if 'HTTP_ACCEPT' in request.META:
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
-        return HttpResponse(json.dumps(json_ld,
-                            ensure_ascii=False, indent=4),
-                            content_type=req_neg.use_response_type + "; charset=utf8")
+        template = loader.get_template('about/recipes.html')
+        context = RequestContext(request,
+                                 {'base_url': base_url})
+        return HttpResponse(template.render(context))
     else:
         # client wanted a mimetype we don't support
         return HttpResponse(req_neg.error_message,
