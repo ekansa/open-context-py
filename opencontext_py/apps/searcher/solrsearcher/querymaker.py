60f9724b5366e2341eb1b2f4c219eef25086c0c0
Eric Kansa <kansaeric@gmail.com>
2015-04-25 11:51:27 -0700

    Bug fix in search, child predicate as first item
    Handles so a predicate that has a parent can be queried

--- opencontext_py/apps/searcher/solrsearcher/querymaker.py
+++ opencontext_py/apps/searcher/solrsearcher/querymaker.py
@@ -291,6 +291,13 @@ class QueryMaker():
                                     act_field_fq = p_slug.replace('-', '_') + '___pred_id'
                             elif entity.item_type == 'uri':
                                 act_field_fq = SolrDocument.ROOT_LINK_DATA_SOLR
+                            elif entity.item_type == 'predicates':
+                                act_field_fq = self.get_parent_item_type_facet_field(entity.uri)
+                                lr = LinkRecursion()
+                                parents = lr.get_jsonldish_entity_parents(entity.uri)
+                                if len(parents) > 1:
+                                    p_slug = parents[-2]['slug']
+                                    act_field_fq = p_slug.replace('-', '_') + '___pred_id'
                             else:
                                 act_field_fq = SolrDocument.ROOT_PREDICATE_SOLR
                         # ---------------------------------------------------
