95c7b7b4e40dc9c7422516dcc2d0aa82f7d41612
Eric Kansa <kansaeric@gmail.com>
2015-06-01 16:26:05 -0700

    table export optimization

--- opencontext_py/apps/exports/exprecords/models.py
+++ opencontext_py/apps/exports/exprecords/models.py
@@ -9,7 +9,6 @@ class ExpCell(models.Model):
     project_uuid = models.CharField(max_length=50, db_index=True)
     row_num = models.IntegerField(db_index=True)  # the row number
     field_num = models.IntegerField()  # the field number
-    record_id = models.CharField(max_length=200, db_index=True)  # id or URI for a record value
     record = models.TextField()  # the actual cell value
     updated = models.DateTimeField(auto_now=True)
 
