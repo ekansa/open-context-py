05101c4c752ce15d40fb312c30a9f2e8ac57bd17
Eric Kansa <kansaeric@gmail.com>
2015-07-03 03:26:58 -0700

    Basic hierarchic tree navigation
    it works!!

--- static/oc/js/edit/hierarchy-tree.js
+++ static/oc/js/edit/hierarchy-tree.js
@@ -2,17 +2,19 @@
  * Functions to explore a list-based hiearchy tree
  */
 var hierarchy_data = {}; // object for containment data in memory
+var hierarchy_objs = {}; // object to hold all of the hiearchies
 var data_loads = 1;
 function hierarchy(parent_id, act_dom_id) {
 	
 	this.act_dom_id = act_dom_id;
 	this.parent_id = parent_id;
-	this.object_prefix = 'tree-' + data_loads;
+	this.object_prefix = 'tree_' + data_loads;
 	this.edit_links = true;
 	this.view_links = true;
 	this.class_subdivide = true;
 	this.data = false;
 	this.button_dom_id = false;
+	this.expanded = true;
 	this.get_data = function(){
 		// ajax request to get the data for this hiearchy
 		this.show_loading();
@@ -59,13 +61,37 @@ function hierarchy(parent_id, act_dom_id) {
 				var html = this.make_data_html(data);
 				act_dom.innerHTML = html;
 			}
+			this.make_collapse_botton();
+			$('#' + this.act_dom_id).collapse('show');
+			this.expanded = true;
 		}
-		this.make_collapse_botton();
+	}
+	this.exec_toggle_collapse = function(){
+		var link_dom_id = this.act_dom_id.replace('-more-', '-expa-');
+		if (this.expanded) {
+			$('#' + this.act_dom_id).collapse('hide');
+			//alert('hidden: ' + link_dom_id);
+			if (document.getElementById(link_dom_id)) {
+				var a_link = document.getElementById(link_dom_id);
+				a_link.innerHTML = '<span class="hierarchy-tog glyphicon glyphicon-plus" aria-hidden="true"></span>';
+			}
+			this.expanded = false;
+		}
+		else{
+			$('#' + this.act_dom_id).collapse('show');
+			//alert('hidden: ' + link_dom_id);
+			if (document.getElementById(link_dom_id)) {
+				var a_link = document.getElementById(link_dom_id);
+				a_link.innerHTML = '<span class="hierarchy-tog glyphicon glyphicon-minus" aria-hidden="true"></span>';
+			}
+			this.expanded = true;
+		}
 	}
 	this.make_data_html = function(data){
 		var html = [];
 		for (var i = 0, length = data.length; i < length; i++) {
-			html.push('<ul class="list-group" style="margin-bottom:5px">');
+			//html.push('<ul class="list-group" style="margin-bottom:5px">');
+			html.push('<ul class="list-unstyled">');
 			var children_html = this.make_children_html(data[i].children, i);
 			html.push(children_html);
 			html.push('</ul>');
@@ -85,7 +111,8 @@ function hierarchy(parent_id, act_dom_id) {
 				var tog_html = '<span class="glyphicon glyphicon-file" aria-hidden="true"></span>';
 				var children_area_html = '';
 			}
-			html.push('<li class="list-group-item" style="border:none; padding:5px;">');
+			//html.push('<li class="list-group-item" style="border:none; padding:5px;">');
+			html.push('<li>');
 			//html.push('<div class="row">');
 			//html.push('<div class="col-sm-1">');
 			html.push(tog_html);
@@ -105,7 +132,7 @@ function hierarchy(parent_id, act_dom_id) {
 		var more_div_dom_id = this.object_prefix + '-more-' + node_i + '-' +index;
 		var tog_html = [
 			'<span id="' + dom_id + '">',
-			'<a title="Click to load more data and expand" ',
+			'<a title="Click to load more data and expand" role="button" ',
 			'onclick="load_expand(\''+ item_id + '\', \''+ more_div_dom_id + '\', \''+ dom_id + '\');">',
 			'<span class="glyphicon glyphicon-plus" aria-hidden="true">',
 			'</span>',
@@ -117,7 +144,7 @@ function hierarchy(parent_id, act_dom_id) {
 	this.make_more_div_html = function(node_i, index){
 		var more_div_dom_id = this.object_prefix + '-more-' + node_i + '-' +index;
 		var html = [
-		'<div id="' + more_div_dom_id + '" class="container collapse" style="margin-top:5px; margin-bottom:5px">',
+		'<div id="' + more_div_dom_id + '" class="tree-cont container collapse" style="margin-top:5px; margin-bottom:5px">',
 		'</div>'
 		].join('\n');
 		return html;
@@ -125,10 +152,13 @@ function hierarchy(parent_id, act_dom_id) {
 	this.make_collapse_botton = function(){
 		if (this.button_dom_id != false) {
 			if (document.getElementById(this.button_dom_id)) {
+				var button_a_id = this.button_dom_id.replace('-exp-', '-expa-');
 				var tog_html = [
 				'<a title="Click to expand" data-toggle="collapse" expanded="true" ',
-				'href="#' + act_dom_id + '">',
-				'<span class="glyphicon glyphicon-plus" aria-hidden="true">',
+				//'href="#' + act_dom_id + '" role="button" class="hierarchy-exp" aria-expanded="true" ',
+				'onclick="collapse_toggle(\'' + this.object_prefix + '\',\'' + act_dom_id + '\')" role="button" class="hierarchy-exp" aria-expanded="true" ',
+				'id="' + button_a_id + '" >',
+				'<span class="glyphicon glyphicon-minus hierarchy-tog" aria-hidden="true">',
 				'</span>',
 				'</a>'
 				].join('\n');
@@ -145,5 +175,16 @@ function load_expand(parent_id, act_dom_id, button_dom_id){
 		var expanded_tree = new hierarchy(parent_id, act_dom_id);
 		expanded_tree.button_dom_id = button_dom_id;
 		expanded_tree.get_data();
+		var tree_key = expanded_tree.object_prefix; 
+		hierarchy_objs[tree_key] = expanded_tree;
+	}
+}
+
+function collapse_toggle(tree_key, act_dom_id){
+	// this calls the appropriate tree object to show or hide a node.
+	if (tree_key in hierarchy_objs) {
+		var tree = hierarchy_objs[tree_key];
+		tree.exec_toggle_collapse();
+		hierarchy_objs[tree_key] = tree;
 	}
 }
