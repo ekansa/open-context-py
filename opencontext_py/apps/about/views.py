bbf1fc16e2ae5d1afa9853138cdc56c35a278b64
Eric Kansa <kansaeric@gmail.com>
2015-10-22 17:35:34 -0700

    People page added

--- opencontext_py/apps/about/views.py
+++ opencontext_py/apps/about/views.py
@@ -79,6 +79,8 @@ def pub_view(request):
                             status=415)
 
 
+# @cache_control(no_cache=True)
+# @never_cache
 def people_view(request):
     """ Get the search context JSON-LD """
     rp = RootPath()
@@ -88,7 +90,7 @@ def people_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
-        template = loader.get_template('about/temp.html')
+        template = loader.get_template('about/people.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - People',
