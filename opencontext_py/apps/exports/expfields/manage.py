5ee9263fb1e73a1850128a47ff2b5b9faaca24cc
Eric Kansa <kansaeric@gmail.com>
2016-04-22 12:33:45 -0700

    More export table field editing methods
    Still to do. More testing....

--- opencontext_py/apps/exports/expfields/manage.py
+++ opencontext_py/apps/exports/expfields/manage.py
@@ -14,16 +14,17 @@ class ExpFieldManage():
     certain fields from an export table. These methods
     accomplish those tasks.
 
-from opencontext_py.apps.exports.exprecords.models import ExpCell
 from opencontext_py.apps.exports.expfields.manage import ExpFieldManage
 exfman = ExpFieldManage()
 table_id = '05f2db65ff4faee1290192bd9a1868ed'
-# exfman.consolidate_same_field_labels(table_id)
-act_fields = [257,258,260]
-act_cells = ExpCell.objects.filter(table_id=table_id,
-                                           field_num__in=act_fields)\
-                           .order_by('row_num')\
-                           .distinct('row_num')
+exfman.change_field_number(table_id, 78, 29)
+
+
+from opencontext_py.apps.exports.expfields.manage import ExpFieldManage
+exfman = ExpFieldManage()
+table_id = 'f07bce4fb08cfe926505c9e534d89a09'
+exfman.change_field_number(table_id, 32, 27)
+
 
     """
 
@@ -108,10 +109,10 @@ act_cells = ExpCell.objects.filter(table_id=table_id,
                                            field_num__in=act_fields)\
                                    .order_by('row_num')\
                                    .distinct('row_num')
-        for act_cell in keep_cells:
+        for act_cell in act_cells:
             # now get cells for this row in the fields to be merged and removed
-            act_row = act_cell.row
-            print('Working on row: ' + str(act_cell.row_num))
+            act_row = act_cell.row_num
+            print('Merge to field: ' + str(keep_field_num) + ', row: ' + str(act_cell.row_num))
             # cell_values is a list of values that are getting merged together
             cell_values = []
             # gets all the cells with values for the keep and remove fields
@@ -138,6 +139,96 @@ act_cells = ExpCell.objects.filter(table_id=table_id,
                            field_num__in=merge_remove_field_nums,
                            row_num=act_row)\
                    .delete()
+
+    def delete_fields_after(self, table_id, last_keep_field_num):
+        """ Deletes field_num greter than the
+            last_keep_field_num
+        """
+        delete_field_nums = []
+        del_fields = ExpField.objects\
+                             .filter(table_id=table_id,
+                                     field_num__gt=last_keep_field_num)
+        for del_field in del_fields:
+            delete_field_nums.append(del_field.field_num)
+        self.delete_fields_list(table_id, delete_field_nums)
+
+    def delete_fields_list(self, table_id, delete_field_nums):
+        """ Deletes fields in a list of field numbers """
+        if isinstance(delete_field_nums, int):
+            # make 1 integer a list
+            delete_field_nums = [delete_field_nums]
+        if isinstance(delete_field_nums, list):
+            if len(delete_field_nums) > 0:
+                # we have a list, so use in a delete queries
+                print('Deleting ' + str(len(delete_field_nums)) + ' fields...')
+                num_del_cells = ExpCell.objects\
+                                       .filter(table_id=table_id,
+                                               field_num__in=delete_field_nums)\
+                                       .delete()
+                print('Deleted ' + str(num_del_cells) + ' cell records.')
+                num_del_fields = ExpField.objects\
+                                         .filter(table_id=table_id,
+                                                 field_num__in=delete_field_nums)\
+                                         .delete()
+                print('Finished deleting ' + str(num_del_fields) + ' fields.')
+                print('Checking and cleaning up table...')
+                self.update_field_numbering(table_id)
+
+    def change_field_number(self, table_id, old_field_num, new_field_num):
+        """ changes the field number to a new field number,
+            this will change field numbering for the whole table
+            so it gets complicated
+        """
+        # first put the field we're moving into a temporary location
+        # for the time being it will be field_num -1
+        num_updated = ExpCell.objects\
+                             .filter(table_id=table_id,
+                                     field_num=old_field_num)\
+                             .update(field_num=-1)
+        print('Staged move of ' + str(num_updated) + ' cell records')
+        ok = ExpField.objects\
+                     .filter(table_id=table_id,
+                             field_num=old_field_num)\
+                     .update(field_num=-1)
+        # now update everything that currently has a new_field_num
+        # and higher to increment up 1 so we make space to for
+        # the field to be moved.
+        # NOTE! Need to order by cells in descending order of field_nums
+        # so we don't get update a cell into a field that's is already occupied
+        move_fields = ExpField.objects\
+                              .filter(table_id=table_id,
+                                      field_num__gte=new_field_num)\
+                              .order_by('-field_num')
+        for move_field in move_fields:
+            act_old_field_num = move_field.field_num
+            act_new_field_num = act_old_field_num + 1
+            # mass update the cell records to add 1 to the field_num
+            num_move_cells = ExpCell.objects\
+                                    .filter(table_id=table_id,
+                                            field_num=act_old_field_num)\
+                                    .update(field_num=act_new_field_num)
+            message = 'Shifted ' + str(num_move_cells) + ' from field: '
+            message += str(act_old_field_num) + ' to: ' + str(act_new_field_num)
+            print(message)
+            move_field.field_num = act_new_field_num
+            move_field.save()
+            print('Completed moving field: ' + str(act_old_field_num) + ' to ' + str(act_new_field_num))
+        # now, finally move the field we set aside to
+        # its new field number
+        num_updated = ExpCell.objects\
+                             .filter(table_id=table_id,
+                                     field_num=-1)\
+                             .update(field_num=new_field_num)
+        print('Completed move of ' + str(num_updated) + ' cell records')
+        ok = ExpField.objects\
+                     .filter(table_id=table_id,
+                             field_num=-1)\
+                     .update(field_num=new_field_num)
+        print('Completed move of field: ' + str(old_field_num) + ' to: ' + str(new_field_num))
+        # now that is done, we do some clean up to make sure
+        # the field numbers have no gaps
+        print('Doing final checks and cleanup...')
+        self.update_field_numbering(table_id)
 
     def update_field_numbering(self, table_id):
         """ updates the numbering for fields after the
