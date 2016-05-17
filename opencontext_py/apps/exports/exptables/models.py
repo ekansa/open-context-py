737a041755fb384299eefc69874552c12d80bc9d
Eric Kansa <kansaeric@gmail.com>
2016-05-16 17:45:09 -0700

    Minor model constraint change

--- opencontext_py/apps/exports/exptables/models.py
+++ opencontext_py/apps/exports/exptables/models.py
@@ -15,8 +15,7 @@ class ExpTable(models.Model):
     field_count = models.IntegerField()
     row_count = models.IntegerField()
     meta_json = JSONField(default={},
-                          load_kwargs={'object_pairs_hook': collections.OrderedDict},
-                          blank=True)
+                          load_kwargs={'object_pairs_hook': collections.OrderedDict})
     short_des = models.CharField(max_length=200)
     abstract = models.TextField()
     created = models.DateTimeField()
