b4193b46b29dbf32f46507a859ea10314c882b59
Eric Kansa <kansaeric@gmail.com>
2016-07-31 06:46:48 -0700

    Still trying to get HTTPS settings to work
    Finding that magic combination that avoids infinite redirections..

--- opencontext_py/settings.py
+++ opencontext_py/settings.py
@@ -93,7 +93,7 @@ DEFAULT_HTTPS = False
 if 'SECURE_SSL_REDIRECT' in secrets:
     secrets_https = get_secret('SECURE_SSL_REDIRECT')
     if secrets_https == 1:
-        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
         SECURE_SSL_REDIRECT = True
         DEFAULT_HTTPS = True
 
