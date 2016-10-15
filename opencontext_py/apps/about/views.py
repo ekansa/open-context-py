827f75cf8cca4fee6cb39caf2851d12f9fcc9bf2
Eric Kansa <ekansa@alexandriaarchive.org>
2016-10-15 14:06:39 -0700

    Open Graph metadata for about / index pages

--- opencontext_py/apps/about/views.py
+++ opencontext_py/apps/about/views.py
@@ -22,11 +22,22 @@ def index_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Video and introduction to Open Context, an open-access '
+                           'data publication service for archaeology ',
+            'image': False,
+            'video': 'https://www.youtube.com/v/qqoPhTZDG04'
+        }
         template = loader.get_template('about/index.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About',
                                   'act_nav': 'about',
+                                  'og': open_graph,
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
     else:
@@ -46,9 +57,21 @@ def uses_view(request):
     if req_neg.supported:
         # requester wanted a mimetype we DO support
         template = loader.get_template('about/uses.html')
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/uses',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Summary of how to use Open Context for sharing, '\
+                           'preserving, exploring and analyzing archaeological '\
+                           'research data',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Uses',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -69,11 +92,22 @@ def pub_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/publishing',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'How to publish archaeological research data '\
+                           'with Open Context',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/publishing.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Publishing',
                                   'act_nav': 'about',
+                                  'og': open_graph,
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
     else:
@@ -93,10 +127,22 @@ def people_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/people',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Data editors, software developers, designers '\
+                           'and alumni with Open Context research data '\
+                           'publishing services',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/people.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - People',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -118,10 +164,22 @@ def estimate_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/estimate',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Estimate data publication and archiving '\
+                           'costs with Open Context to help budget for '\
+                           'grant data management plans',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/estimate.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Cost Estimate',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -179,11 +237,23 @@ def tech_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/technology',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Overview of the open-source software technologies '\
+                           'created and used by Open Context to publish '\
+                           'archaeological data on the Web',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/technology.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Technology',
                                   'act_nav': 'about',
+                                  'og': open_graph,
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
     else:
@@ -201,10 +271,22 @@ def services_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/technology',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Overview of the APIs (machine-readable data) '\
+                           'offered by Open Context to promote '\
+                           'interoperability and new uses of data',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/services.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Web Services and APIs',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -223,10 +305,22 @@ def recipes_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/recipes',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Specific guidance on the use Open Context APIs '\
+                           '(machine-readable data) to meet certain data '\
+                           'management needs',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/recipes.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - API Cookbook',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -246,10 +340,23 @@ def bibliography_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/bibliography',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Publications related to Open Context and its '\
+                           'contributions to research data management, '\
+                           'archaeological ethics, scholarly communications, and '\
+                           'professional practice',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/bibliography.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Bibliography',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -269,10 +376,22 @@ def ip_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/intellectual-property',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Intellectual property policies for Open Context and '\
+                           'ethical guidance for contributors and users of '\
+                           'archaeological research data',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/intellectual-property.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Intellectual Property',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
@@ -293,10 +412,22 @@ def sponsors_view(request):
         req_neg.check_request_support(request.META['HTTP_ACCEPT'])
     if req_neg.supported:
         # requester wanted a mimetype we DO support
+        open_graph = {
+            'twitter_site': settings.TWITTER_SITE,
+            'type': 'website',
+            'url': base_url + '/about/sponsors',
+            'site_name': settings.CANONICAL_SITENAME,
+            'description': 'Sources of financial support for '\
+                           'Open Context and collaborative institutions providing '\
+                           'complementary services',
+            'image': base_url + '/static/oc/images/index/oc-blue-square-logo.png',
+            'video': False
+        }
         template = loader.get_template('about/sponsors.html')
         context = RequestContext(request,
                                  {'base_url': base_url,
                                   'page_title': 'Open Context: About - Intellectual Property',
+                                  'og': open_graph,
                                   'act_nav': 'about',
                                   'nav_items': settings.NAV_ITEMS})
         return HttpResponse(template.render(context))
