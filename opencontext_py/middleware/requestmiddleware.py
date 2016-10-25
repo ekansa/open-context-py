e6386b0e720d4ff9aec904af2d8f750be7d6a732
Eric Kansa <ekansa@alexandriaarchive.org>
2016-10-25 13:41:09 -0700

    More debugging attempts

--- opencontext_py/middleware/requestmiddleware.py
+++ opencontext_py/middleware/requestmiddleware.py
@@ -13,16 +13,24 @@ class RequestMiddleware(object):
     def __init__(self, get_response=None):
         self.start_time = None
         self.get_response = get_response
+        self.content_type = 'text/html'
         # One-time configuration and initialization.
     
     def process_request(self, request):
         self.start_time = time.time()
+        request.start_time = self.start_time 
         request.content_type = 'text/html'  # default, unless changed elsewhere
     
     def process_response(self, request, response):
         r_metric = RequestHttpMetric(request)
-        r_metric.time_start = self.start_time
-        r_metric.mime_type = request.content_type
+        if hasattr(request, 'start_time'):
+            r_metric.time_start = request.start_time
+        else:
+            r_metric.time_start = self.start_time
+        if hasattr(request, 'content_type'):
+            r_metric.mime_type = request.content_type
+        else:
+            r_metric.mime_type = self.content_type
         r_metric.record()
         return response
 
