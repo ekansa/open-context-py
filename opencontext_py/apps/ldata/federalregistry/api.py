d1150e8d0e9ee5133bc4d7696b765d96157b2eb7
Eric Kansa <ekansa@alexandriaarchive.org>
2016-11-08 13:16:19 -0800

    Start on Federal registry API
    Not really a start, just basically a copy of the tDAR one.

---
+++ opencontext_py/apps/ldata/federalregistry/api.py
@@ -0,0 +1,147 @@
+import json
+import requests
+import feedparser
+from time import sleep
+from django.utils.http import urlquote, quote_plus, urlquote_plus
+from opencontext_py.libs.general import LastUpdatedOrderedDict
+from opencontext_py.libs.generalapi import GeneralAPI
+
+
+class FederalRegistryAPI():
+    """ Interacts with the Federal Registry API
+        Fo relate DINAA trinomials with
+        Federal Registry documents
+    """
+    API_BASE_URL = 'http://www.federalregister.gov/api/v1/documents.json'
+    SLEEP_TIME = .5
+
+    def __init__(self):
+        self.request_error = False
+        self.request_url = False
+        self.results = False
+        self.best_match = False
+        self.html_url = False
+        self.delay_before_request = self.SLEEP_TIME
+
+    def get_site_keyword(self, site_keyword):
+        """ get a key word for a site """
+        results = False
+        json_r = self.get_keyword_search_json(site_keyword,
+                                              'SiteNameKeyword')
+        if isinstance(json_r, dict):
+            if 'items' in json_r:
+                if len(json_r['items']) > 0:
+                    results = []
+                    for item in json_r['items']:
+                        result = {'label': False,
+                                  'id': False,
+                                  'tdar': item}
+                        if 'label' in item:
+                            # get the item label
+                            result['label'] = item['label']
+                        if 'detailUrl' in item:
+                            # make a full URI for the keyword
+                            result['id'] = self.BASE_URI + item['detailUrl']
+                        if result['id'] is not False \
+                           and result['label'] is not False:
+                            # Data is complete, so we can use it. Append to results
+                            results.append(result)
+        if results is not False:
+            self.best_match = results[0]
+        return results
+
+    def get_keyword_search_json(self, keyword, keyword_type):
+        """
+        gets json data from tDAR in response to a keyword search
+        """
+        if self.delay_before_request > 0:
+            # default to sleep BEFORE a request is sent, to
+            # give the remote service a break.
+            sleep(self.delay_before_request)
+        payload = {'term': keyword,
+                   'keywordType': keyword_type}
+        url = self.KEYWORD_API_BASE_URL
+        try:
+            gapi = GeneralAPI()
+            r = requests.get(url,
+                             params=payload,
+                             timeout=240,
+                             headers=gapi.client_headers)
+            self.request_url = r.url
+            r.raise_for_status()
+            json_r = r.json()
+        except:
+            self.request_error = True
+            json_r = False
+        return json_r
+
+    def get_tdar_items_by_site_keyword_objs(self, keyword_objs):
+        """ gets site information by tdar keyword objects """
+        output = False
+        keyword_uris = []
+        if isinstance(keyword_objs, list):
+            for keyword_obj in keyword_objs:
+                if isinstance(keyword_obj, dict):
+                    if 'id' in keyword_obj:
+                        keyword_uris.append(keyword_obj['id'])
+        if len(keyword_uris) > 0:
+            output = self.search_by_site_keyword_uris(keyword_uris,
+                                                      True)
+        return output
+
+    def search_by_site_keyword_uris(self,
+                                    keyword_uris,
+                                    add_templating_keys=False):
+        """
+        sets an Atom feed (sorta) from tDAR related to site
+        keywords
+        """
+        output = False
+        site_names = []
+        if not isinstance(keyword_uris, list):
+            keyword_uris = [keyword_uris]
+        for keyword_uri in keyword_uris:
+            if self.BASE_URI in keyword_uri \
+               and 'site-name' in keyword_uri:
+                # is a tDAR site-name URI
+                uri_ex = keyword_uri.split('/')
+                site_name = uri_ex[-1]  # last part of URI is the site-name
+                site_names.append(site_name)
+        if len(site_names) > 0:
+            feed_url = self.SITE_SEARCH_FEED_BASE_URL
+            self.html_url = self.SITE_SEARCH_HTML_BASE_URL
+            # Add a bunch of query parameters
+            params = '?_tdar.searchType=advanced&sortField=RELEVANCE&groups%5B0%5D'
+            params += '.operator=AND&groups%5B0%5D.fieldTypes%5B0%5D=FFK_SITE'
+            feed_url += params
+            self.html_url += params
+            i = 0
+            for site_name in site_names:
+                act_param = '&groups%5B0%5D.siteNames%5B' + str(i) + '%5D='
+                act_param += urlquote(site_name)
+                feed_url += act_param
+                self.html_url += act_param
+                i += 1
+            feed = feedparser.parse(feed_url)
+            # print(feed_url)
+            if feed.bozo == 1 \
+               or feed.status >= 400:
+                feed = False
+            else:
+                output = []
+                for entry in feed.entries:
+                    item = LastUpdatedOrderedDict()
+                    item['id'] = entry.id
+                    item['label'] = entry.title
+                    if add_templating_keys:
+                        # keys useful for templating the tDAR content
+                        item['vocabulary'] = 'tDAR archived content'
+                        item['vocab_uri'] = 'http://tdar.org/'
+                        item['vocabulary'] = False
+                        item['vocab_uri'] = False
+                    output.append(item)
+        return output
+
+    def pause_request(self):
+        """ pauses between requests """
+        sleep(self.delay_before_request)
---
+++ opencontext_py/apps/ldata/federalregistry/dinaalink.py
@@ -0,0 +1,162 @@
+import sys
+from time import sleep
+from opencontext_py.apps.ldata.tdar.api import tdarAPI
+from opencontext_py.apps.edit.dinaa.trinomials.models import Trinomial
+from opencontext_py.apps.edit.dinaa.trinomials.manage import TrinomialManage
+from opencontext_py.apps.ocitems.manifest.models import Manifest
+from opencontext_py.apps.ldata.linkentities.models import LinkEntity
+from opencontext_py.apps.ldata.linkannotations.models import LinkAnnotation
+
+
+class dinaaLink():
+    """ methods to link Smithsonian Trinomials curated by
+        DINAA with keywords in tDAR
+    """
+    DC_TERMS_SUBJECT = 'dc-terms:subject'
+    TDAR_VOCAB = 'http://core.tdar.org/browse/site-name/'
+
+    def __init__(self):
+        self.request_error = False
+        self.lead_zero_check = True
+        self.max_results = 3
+        self.error_wait = 0  # wait if problem to send next request
+        self.base_wait = 300
+        self.max_wait = self.base_wait * 5
+
+    def match_dinaa_ids(self, limit=False):
+        """ get a key word for a site """
+        found_matches = 0
+        if limit is not False:
+            tris = Trinomial.objects.filter(trinomial__isnull=False,
+                                            tdar_checked__isnull=True)[:limit]
+        else:
+            tris = Trinomial.objects.filter(trinomial__isnull=False,
+                                            tdar_checked__isnull=True)
+        len_tris = len(tris)
+        i = 1
+        for tri in tris:
+            found_matches += self.match_trinomial_obj(tri)
+            if self.request_error is False:
+                tri.tdar_checked_save()
+                print('Total tDAR matches: ' + str(found_matches) + ', Checked item: ' + str(i) + ' of ' + str(len_tris))
+            i += 1
+        return found_matches
+
+    def match_trinomial_obj(self, tri):
+        """ Attempts to match a trinomial object 'tri'
+            against tDAR, if it hasn't yet been matched
+        """
+        found_matches = 0
+        manifest = False
+        try:
+            manifest = Manifest.objects.get(uuid=tri.uuid)
+        except Manifest.DoesNotExist:
+            manifest = False
+        la_check = LinkAnnotation.objects\
+                                 .filter(subject=tri.uuid,
+                                         predicate_uri='dc-terms:subject',
+                                         object_uri__contains=self.TDAR_VOCAB)[:1]
+        if len(la_check) < 1 and manifest is not False:
+            # we don't already have a tDAR id for this item, continue with matches
+            tri_man = TrinomialManage()
+            request_keywords = [tri.trinomial]
+            if self.lead_zero_check:
+                # check multiple leading zeros
+                tri_parts = tri_man.parse_trinomial(tri.trinomial)
+                site = tri_parts['site']
+                site_part_len = len(site)
+                while len(site) < 4:
+                    site = '0' + site
+                    new_trinomial = tri_parts['state'] + tri_parts['county'] + site
+                    request_keywords.append(new_trinomial)
+            for keyword in request_keywords:
+                tdar_api = tdarAPI()
+                results = tdar_api.get_site_keyword(keyword)
+                if isinstance(results, list):
+                    for result in results[:self.max_results]:
+                        # assume it is a spurious match
+                        match_real = False
+                        if result['label'] == tri.trinomial:
+                            # the trinomial and the tDAR result exactly match
+                            match_real = True
+                        else:
+                            # check if the only difference is in leading zeros
+                            tri_parts = tri_man.parse_trinomial(tri.trinomial)
+                            site = tri_parts['site']
+                            site_part_len = len(site)
+                            while len(site) < 5:
+                                site = '0' + site
+                                new_trinomial = tri_parts['state'] + tri_parts['county'] + site
+                                if new_trinomial == result['label']:
+                                    # A good match, the tDAR result and the trinomial
+                                    # match (but with different leading zeros)
+                                    match_real = True
+                        if match_real:
+                            found_matches += 1
+                            # OK! Found a match, first save the linked entity in the link entity table
+                            le_check = False
+                            try:
+                                le_check = LinkEntity.objects.get(uri=result['id'])
+                            except LinkEntity.DoesNotExist:
+                                le_check = False
+                            if le_check is False:
+                                le = LinkEntity()
+                                le.uri = result['id']
+                                le.label = result['label']
+                                le.alt_label = result['label']
+                                le.vocab_uri = self.TDAR_VOCAB
+                                le.ent_type = 'type'
+                                le.save()
+                            # Now save the link annotation
+                            la = LinkAnnotation()
+                            la.subject = tri.uuid
+                            la.subject_type = manifest.item_type
+                            la.project_uuid = manifest.project_uuid
+                            la.source_id = 'tdar-api-lookup'
+                            la.predicate_uri = self.DC_TERMS_SUBJECT
+                            la.object_uri = result['id']
+                            la.save()
+                        else:
+                            print('Almost! ' + result['label'] + ' is not exactly: ' + tri.trinomial)
+                if tdar_api.request_error:
+                    self.request_error = True
+                    print('HTTP request to tDAR failed!')
+                    self.error_wait += self.base_wait
+                    if self.error_wait > self.max_wait:
+                        print('Too many failures, quiting...')
+                        sys.exit('Quitting process')
+                    else:
+                        # sleep some minutes before trying again
+                        print('Will try again in ' + str(self.error_wait) + ' seconds...')
+                        sleep(self.error_wait)
+                else:
+                    self.request_error = False
+                    if self.error_wait >= self.base_wait:
+                        print('HTTP requests resumed OK, will continue.')
+                        self.error_wait = 0
+        return found_matches
+
+    def cache_db_tdar_refs(self, overwrite=True):
+        """ caches in the database items in tDAR that cross-reference
+            with Open Context plances.
+            
+            Calls the tDAR API to do so
+        """
+        if overwrite:
+            tdar_annos = LinkAnnotation.objects\
+                                       .filter(subject_type='subjects',
+                                               object_uri__contains=tdarAPI.BASE_URI)
+        else:
+            tdar_annos = LinkAnnotation.objects\
+                                       .filter(subject_type='subjects',
+                                               object_uri__contains=tdarAPI.BASE_URI,
+                                               obj_extra=None)
+        for tdar_anno in tdar_annos:
+            print('Checking: ' + str(tdar_anno.object_uri))
+            tdar = tdarAPI()
+            tdar.pause_request()
+            tdar_items = tdar.search_by_site_keyword_uris(tdar_anno.object_uri)
+            if isinstance(tdar_items, list):
+                print('Found: ' + str(len(tdar_items)) + ' items')
+                tdar_anno.obj_extra = {'dc-terms:isReferencedBy': tdar_items}
+                tdar_anno.save()
