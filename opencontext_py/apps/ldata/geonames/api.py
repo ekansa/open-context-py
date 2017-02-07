33d57fd55acb63c348bebe6818b4abe959595a08
Eric Kansa <ekansa@alexandriaarchive.org>
2017-02-06 16:29:30 -0800

    Added methods to relate to Geonames
    Open Context has several geospatial entities that directly relate to
    Geonames. These automate their lookup.

--- opencontext_py/apps/ldata/geonames/api.py
+++ opencontext_py/apps/ldata/geonames/api.py
@@ -7,7 +7,8 @@ from opencontext_py.apps.ldata.linkentities.models import LinkEntityGeneration
 
 
 class GeonamesAPI():
-    """ Interacts with Periodo """
+    """ Interacts with Geonames """
+    SEARCH_BASE_URL = 'http://api.geonames.org/searchJSON'
     JSON_BASE_URL = 'http://www.geonames.org/getJSON?id='
     VOCAB_URI = 'http://www.geonames.org'
     SLEEP_TIME = .5
@@ -58,3 +59,58 @@ class GeonamesAPI():
             json_r = False
         self.json_data = json_r
         return self.json_data
+    
+    def search_admin_entity(self,
+                            q_str,
+                            admin_level=0,
+                            username='demo',
+                            lat=None,
+                            lon=None,
+                            degree_dif=.5):
+        """ searches for an entity of a given administrative type
+            associated for a given q_str
+        """
+        output = None
+        all_params = {}
+        all_params['q'] = q_str
+        all_params['username'] = username
+        all_params['maxRows'] = 1
+        if isinstance(lat, float) \
+           and isinstance(lon, float) \
+           and isinstance(degree_dif, float):
+            all_params['east'] = lon - degree_dif
+            all_params['west'] = lon + degree_dif
+            all_params['south'] = lat - degree_dif
+            all_params['north'] = lat + degree_dif
+        if admin_level == 0:
+            fcodes = ['PCLI',
+                      'OCN']
+        elif admin_level == 1:
+            fcodes = ['ADM1']
+        elif admin_level == 2:
+            fcodes = ['ADM2']
+        else:
+            fcodes = [None]
+        for fcode in fcodes:
+            params = all_params
+            if isinstance(fcode, str):
+                params['fcode'] = fcode
+            if self.delay_before_request > 0:
+                # default to sleep BEFORE a request is sent, to
+                # give the remote service a break.
+                sleep(self.delay_before_request)
+            try:
+                gapi = GeneralAPI()
+                r = requests.get(self.SEARCH_BASE_URL,
+                                 params=params,
+                                 timeout=10,
+                                 headers=gapi.client_headers)
+                r.raise_for_status()
+                print('Checking: ' + r.url)
+                json_r = r.json()
+            except:
+                json_r = False
+            if json_r is not False:
+                output = json_r
+                break
+        return output
