f3be262545de9ccca7b935653b4d2598f6bc40d6
Eric Kansa <ekansa@alexandriaarchive.org>
2018-05-04 16:34:58 -0700

    Minor edit

--- opencontext_py/apps/archive/metadata.py
+++ opencontext_py/apps/archive/metadata.py
@@ -31,7 +31,6 @@ class ArchiveMetadata():
             'Open Context',
             'Data Publication',
             'Structured Data',
-            'Linked Open Data',
             'GeoJSON',
             'JSON-LD'
         ]
