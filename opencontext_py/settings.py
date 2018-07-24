6a85f083a410e6aad7fdd564fdba07af5e9b6c82
Eric Kansa <ekansa@alexandriaarchive.org>
2018-07-24 05:45:31 -0700

    Enlarge Redis Cache

--- opencontext_py/settings.py
+++ opencontext_py/settings.py
@@ -319,7 +319,7 @@ else:
             'LOCATION': 'redis://127.0.0.1:6379/1',
             'TIMEOUT': (60 * 60),  # 1 hour for cache
             'OPTIONS': {
-                'MAX_ENTRIES': 15000,
+                'MAX_ENTRIES': 25000,
                 'CLIENT_CLASS': 'django_redis.client.DefaultClient'
             }
         },
