ca853aa0a93e47951dacc4737f21b6bb7564dfe5
Raymond Yee <raymond.yee@gmail.com>
2018-11-03 05:20:16 +0000

    OK -- still trying to figure out the basics of pytest-django

--- opencontext_py/tests/test_basic.py
+++ opencontext_py/tests/test_basic.py
@@ -1,5 +1,15 @@
 import pytest
 
+from django.test.utils import setup_test_environment
+from django.test.client import Client
+
 def test_hello():
     assert True
+
+def test_main_page():
+
+    client = Client()
+
+    response = client.get('/')
+    assert response.status_code == 200
 
