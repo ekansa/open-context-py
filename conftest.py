5169ead07c32c309f2397b9f333a72e3b8bc991f
Raymond Yee <raymond.yee@gmail.com>
2018-11-05 11:25:56 -0800

    try to use existing db in tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,20 @@
 import pytest
+import json
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
         pass
 
+@pytest.fixture(scope='session')
+def django_db_setup():
+
+	secrets = json.loads('secrets.json')
+
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': secrets.get('DATABASES_NAME'),
+        'USER': secrets.get('DATABASES_USER'),
+        'PASSWORD': secrets.get('DATABASES_PASSWORD'),
+        'HOST': secrets,get('DATABASES_HOST'),
+        'CONN_MAX_AGE': 600,
+    }
