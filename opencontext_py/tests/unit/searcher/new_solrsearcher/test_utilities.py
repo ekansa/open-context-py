5bd3bb37632997739f118a382537244df4c5890f
Eric Kansa <ekansa@alexandriaarchive.org>
2019-10-06 14:12:25 -0700

    Added utility function and unit tests

--- opencontext_py/tests/unit/searcher/new_solrsearcher/test_utilities.py
+++ opencontext_py/tests/unit/searcher/new_solrsearcher/test_utilities.py
@@ -5,6 +5,56 @@ from opencontext_py.apps.searcher.new_solrsearcher import utilities
 logger = logging.getLogger("tests-unit-logger")
 
 
+TESTS_MUTIPLE_OR_PATHS = [
+    # Tuples of test cases, with input strings and expected output lists:
+    #
+    # (raw_path, expected_paths_list, hiearchy_delim, or_delim,),
+    #
+    (
+        'United States/California',
+        ['United States/California'],
+        '/',
+        '||',
+    ),
+    (
+        'Turkey/Domuztepe/I||II||Stray',
+        ['Turkey/Domuztepe/I', 'Turkey/Domuztepe/II', 'Turkey/Domuztepe/Stray'],
+        '/',
+        '||',
+    ),
+    (
+        'foo---bar',
+        ['foo---bar'],
+        '---',
+        '||',
+    ),
+    (
+        'foo---bar||bar',
+        ['foo---bar'],
+        '---',
+        '||',
+    ),
+    (
+        'foo||foo---bar||bar',
+        ['foo---bar'],
+        '---',
+        '||',
+    ),
+    (
+        'foo---bar||bad',
+        ['foo---bar', 'foo---bad'],
+        '---',
+        '||',
+    ),
+    (
+        'foo---bar||bad---super',
+        ['foo---bar---super', 'foo---bad---super'],
+        '---',
+        '||',
+    ),
+]
+
+
 TESTS_FULLTEXT_TO_SOLR = [
     # Tuples of test cases, with input strings and expected output lists:
     #
@@ -20,6 +70,17 @@ TESTS_FULLTEXT_TO_SOLR = [
 ]
 
 
+def test_infer_multiple_or_hierarchy_paths():
+    """Tests creation of multiple hierarchy paths inferred from OR operators"""
+    for raw_path, exp_paths, hierarchy_delim, or_delim in TESTS_MUTIPLE_OR_PATHS:
+        paths_list = utilities.infer_multiple_or_hierarchy_paths(
+            raw_path,
+            hierarchy_delim=hierarchy_delim,
+            or_delim=or_delim,
+        )
+        assert paths_list == exp_paths
+
+
 def test_prep_string_search_term_list():
     """Tests translation of client request to sorting for solr query."""
     for raw_search_text, expected_solr_list in TESTS_FULLTEXT_TO_SOLR:
@@ -27,3 +88,4 @@ def test_prep_string_search_term_list():
             raw_search_text
         )
         assert solr_list == expected_solr_list
+    
