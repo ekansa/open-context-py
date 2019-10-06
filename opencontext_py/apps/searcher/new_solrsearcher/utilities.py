1e43c1f1e4b7dba47cc993c919f224afbcaa7a6f
Eric Kansa <ekansa@alexandriaarchive.org>
2019-10-06 14:12:25 -0700

    Added utility function and unit tests

--- opencontext_py/apps/searcher/new_solrsearcher/utilities.py
+++ opencontext_py/apps/searcher/new_solrsearcher/utilities.py
@@ -1,11 +1,59 @@
 import copy
+import itertools
 import re
 
 from opencontext_py.libs.general import LastUpdatedOrderedDict
 
+# ---------------------------------------------------------------------
+# This module contains general utility functions for the solr search
+# and query features.
+#
+# NOTE: The functions here must not access the database or solr. These
+# functions will be independent of all DB interactions, so they can be
+# tested with unit testing.
+# ---------------------------------------------------------------------
 
-# This module contains general utility functions for the solr search and query
-# features.
+
+def infer_multiple_or_hierarchy_paths(raw_path, hierarchy_delim='/', or_delim='||'):
+    '''Takes a raw path and returns a list of all combinations of paths
+    infered from an OR operator.
+
+    For example:
+
+    >>> infer_multiple_or_hierarchy_paths('Turkey/Domuztepe/I||II||Stray')
+
+    ['Turkey/Domuztepe/I', 'Turkey/Domuztepe/II', 'Turkey/Domuztepe/Stray']
+
+
+    :param str raw_path: The raw path string as requested by the client
+    :param str hierarchy_delim: The hierarchy delimiter.
+    :param str or_delim: The OR operator / delimiter.
+    '''
+    # Split the raw_path by hiearchy delim (default to '/') and then by
+    # the or_delim (default to '||').
+    path_lists = [
+        path_parts.split(or_delim)
+        for path_parts in raw_path.split(hierarchy_delim)
+    ]
+    # Create a list of the various permutations
+    path_tuple_list = list(itertools.product(*path_lists))
+    # Turn the paths a list of unique strings
+    paths = []
+    for path_parts in path_tuple_list:
+        new_path = hierarchy_delim.join(path_parts)
+        if new_path in paths:
+            continue
+        paths.append(new_path)
+    return paths
+
+
+def join_solr_query_terms(terms_list, operator='AND'):
+    """Joins together a list of query terms into a string."""
+    if not isinstance(terms_list, list):
+        terms_list = [terms_list]
+    terms = ['({})'.format(term) for term in terms_list]
+    terms_str = (' {} '.format(operator)).join(terms)
+    return '({})'.format(terms_str)
 
 
 def escaped_seq(term):
@@ -44,44 +92,82 @@ def escape_solr_arg(arg):
     arg = arg.replace('\\', r'\\')   # escape \ first
     return "".join([next_str for next_str in escaped_seq(arg)])
 
+def string_to_float(str_val, invalid_output=None):
+    """Convert a string value to a float, if valid
+    
+    :param str str_val: String value we want to convert to float
+    """
+    try:
+        output = float(str_val)
+    except:
+        return invalid_output
+    return output
+
+
+def string_to_int(str_val, invalid_output=None):
+    """Convert a string value to a integer, if valid
+    
+    :param str str_val: String value we want to convert to int
+    """
+    float_val = string_to_float(str_val, invalid_output)
+    if float_val == invalid_output:
+        return invalid_output
+    try:
+        output = int(float_val)
+    except:
+        return invalid_output
+    return output
+    
 
 def get_request_param_value(
     request_dict,
     param,
     default,
     as_list=False,
-    solr_escape=False
+    solr_escape=False,
+    require_float=False,
+    require_int=False,
 ):
-    """ get a string or list to use in queries from either
-        the request object or the internal_request object
-        so we have flexibility in doing searches without
-        having to go through HTTP
+    """ Return a list, str, float, or int (dependig on args) from a
+        request dict:
+    
+    :param dict request_dict: The dictionary of keyed by client
+        request parameters and their request parameter values.
+    :param str param: The URL query parameter used as a key for to
+        find client requested values in the request_dict
+    :param * default: The default value to be returned if the param
+        key does not exist in the request_dict
+    :param bool as_list: Boolean to return a list or single value.
+    :param solr_escape: Boolean for solr-escaping input values
+    :param require_float: Boolean to require returned values to be
+        of type float.
+    :param require_int: Boolean to require returned values to be of
+        type int.
     """
     if not isinstance(request_dict, dict):
         return None
     
-    raw_val = request_dict.get(param, default)
-    if not as_list:
-        if isinstance(raw_val, list):
-            output = raw_val[0]
-        else:
-            output = raw_val
+    raw_vals = request_dict.get(param, default)
+    if not isinstance(raw_vals, list):
+        raw_vals = [raw_vals]
+    outputs = []
+    for raw_val in raw_vals:
         if solr_escape:
-            if output[0] == '"' and output[-1] == '"':
-                output = '"{}"'.format(escape_solr_arg(output[1:-1]))
+            if raw_val[0] == '"' and raw_val[-1] == '"':
+                raw_val = '"{}"'.format(escape_solr_arg(raw_val[1:-1]))
             else:
-                output = escape_solr_arg(output)
-        # Return the non-list output and skip the rest of the
-        # function.
-        return output
-    # The rest is for outputs requested as a list.
-    if not isinstance(raw_val, list):
-        if solr_escape:
-            raw_val = '"{}"'.format(escape_solr_arg(raw_val))
-        # 
-        return [raw_val]
-    else:
-        return raw_val
+                raw_val = escape_solr_arg(raw_val)
+        elif require_float:
+            raw_val = string_to_float(raw_val, invalid_output=default)
+        elif require_int:
+            raw_val = string_to_int(raw_val, invalid_output=default)
+        outputs.append(raw_val)
+    
+    if not as_list:
+        # We don't want the output returned as a list, so just return
+        # the first element of the list as the output 
+        return outputs[0]
+    return outputs
 
 
 def prep_string_search_term_list(raw_fulltext_search):
