ff271a7b961e4822752c21249fbcd1e4e737767e
Eric Kansa <ekansa@alexandriaarchive.org>
2019-10-13 15:54:25 -0700

    Added regression tests for spatial context solr query generation

---
+++ opencontext_py/tests/regression/searcher/new_solrsearcher/test_querymaker.py
@@ -0,0 +1,86 @@
+import pytest
+import logging
+import random
+
+from opencontext_py.apps.indexer.solrdocumentnew import SolrDocumentNew as SolrDocument
+
+from opencontext_py.apps.searcher.new_solrsearcher import querymaker
+
+logger = logging.getLogger("tests-regression-logger")
+
+
+TESTS_SPATIAL_CONTEXTS = [
+    # Tuples of test cases, with input spatial context path
+    # and expected output dicts:
+    #
+    # (spatial_context, expected_query_dict),
+    #
+    (
+        None,
+        {
+           'fq':[],
+           'facet.field':[SolrDocument.ROOT_CONTEXT_SOLR],
+        },
+    ),
+    (
+        'United+States',
+        {
+           'fq':['(root___context_id_fq:united-states)'],
+           'facet.field':['united_states___context_id'],
+        },
+    ),
+    (
+        'United States',
+        {
+           'fq':['(root___context_id_fq:united-states)'],
+           'facet.field':['united_states___context_id'],
+        },
+    ),
+    (
+        'United States/',
+        {
+           'fq':['(root___context_id_fq:united-states)'],
+           'facet.field':['united_states___context_id'],
+        },
+    ),
+    (
+        '/United States',
+        {
+           'fq':['(root___context_id_fq:united-states)'],
+           'facet.field':['united_states___context_id'],
+        },
+    ),
+    (
+        '/United States||',
+        {
+           'fq':['(root___context_id_fq:united-states)'],
+           'facet.field':['united_states___context_id'],
+        },
+    ),
+    (
+        'United States/California',
+        {
+           'fq':['(united_states___context_id_fq:california)'],
+           'facet.field':['california___context_id'],
+        },
+    ),
+    (
+        'United States/California||Florida',
+        {
+           'fq':['((united_states___context_id_fq:california) OR (united_states___context_id_fq:florida))'],
+           'facet.field':['california___context_id', 'florida___context_id',],
+        },
+    ),
+]
+
+
+@pytest.mark.django_db
+def test_get_spatial_context_query_dict():
+    """Tests get_spatial_context_query_dict on a variety of inputs."""
+    for spatial_context, exp_dict in TESTS_SPATIAL_CONTEXTS:
+        query_dict = querymaker.get_spatial_context_query_dict(
+            spatial_context
+        )
+        assert query_dict['fq'] == exp_dict['fq']
+        assert query_dict['facet.field'] == exp_dict['facet.field']
+
