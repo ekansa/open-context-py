6a784a7e024b3d908932e2eff92d261a8f5411e0
Eric Kansa <kansaeric@gmail.com>
2021-02-21 16:07:47 -0800

    Added production to default (local) db sync function

--- opencontext_py/apps/all_items/editorial/synchronize/safe_model_save.py
+++ opencontext_py/apps/all_items/editorial/synchronize/safe_model_save.py
@@ -90,8 +90,8 @@ def prod_safe_save_model_object(model_object, raise_on_error=True):
     return ok
 
 
-def local_safe_save_model_object(model_object, raise_on_error=True):
-    """Safely save a model object from the PROD to the LOCAL database"""
+def default_safe_save_model_object(model_object, raise_on_error=True):
+    """Safely save a model object from the PROD to the DEFAULT database"""
     if not settings.CONNECT_PROD_DB:
         if raise_on_error:
             raise RuntimeError('No "prod" database connection config.')
@@ -104,29 +104,12 @@ def local_safe_save_model_object(model_object, raise_on_error=True):
     try:
         with transaction.atomic():
             # NOTE: This allows updates, not just inserts, 
-            # so as to sync data from prod locally.
+            # so as to sync data from prod to the default (usually local)
             model_object.save(using='default')
             ok = True
     except Exception as e:
         ok = False
     return ok
-
-
-def check_prod_model_exists(model_object, raise_on_error=True):
-    """Check if the model exists on the prod server"""
-    if not settings.CONNECT_PROD_DB:
-        if raise_on_error:
-            raise RuntimeError('No "prod" database connection config.')
-        return None
-    if model_object.pk is None:
-        if raise_on_error:
-            raise ValueError('Model object has no "primary key" cannot safely insert.')
-        return None
-    cache = caches['redis']
-    cache_key = str(model_object.pk)
-    prod_exists = cache.get(cache_key)
-    if not prod_exists:
-        prod_obj = AllManifest.objects.filter(pk=model_object.pk)
 
 
 def prod_safe_save_model_object_and_related(model_object, raise_on_error=True):
@@ -182,6 +165,65 @@ def prod_safe_save_model_object_and_related(model_object, raise_on_error=True):
     # 'prod' database, because ultimately, those default objects give context
     # to other objects.
     ok = prod_safe_save_model_object(model_object, raise_on_error=raise_on_error)
+    if ok and act_model == AllManifest:
+        # Only set the cache if this is a manifest item,
+        # which we hit often b/c of foreign key relations
+        cache.set(cache_key, True, timeout=DB_SYNC_CACHE_LIFE)
+    return ok
+
+
+def default_safe_save_model_object_and_related(model_object, raise_on_error=True):
+    """Safely save a model object, and foreign key related objects to the DEFAULT database"""
+    if not settings.CONNECT_PROD_DB:
+        if raise_on_error:
+            raise RuntimeError('No "prod" database connection config.')
+        return False
+    if model_object.pk is None:
+        if raise_on_error:
+            raise ValueError('Model object has no "primary key" cannot safely insert.')
+        return False
+
+    cache = caches['redis']
+    cache_key = f'default-pk-{str(model_object.pk)}'
+    default_exists = cache.get(cache_key)
+    if default_exists:
+        # Our work is done, we know this already exists on default 
+        # because we cached our knowledge that it does exist.
+        return True
+    
+    act_model = model_object._meta.model
+
+    # Check the production database to see if this object already exists there.
+    default_obj = act_model.objects.using('default').filter(pk=model_object.pk).first()
+    if default_obj:
+        # The default db actually already have this item. Cache our new
+        # knowledge of this so we can avoid future DB lookups.
+        if act_model == AllManifest:
+            # Only set the cache if this is a manifest item,
+            # which we hit often b/c of foreign key relations
+            cache.set(cache_key, True, timeout=DB_SYNC_CACHE_LIFE)
+        return True
+
+    # OK we actually have not saved this on prod, so do it.
+    fk_attribs = MODEL_FK_MANIFEST_ATTRIBUTES.get(act_model, [])
+    all_fk_ok = True
+    for fk_attrib in fk_attribs:
+        fk_obj = getattr(model_object, fk_attrib)
+        fk_ok = default_safe_save_model_object_and_related(fk_obj)
+        if not fk_ok:
+            # Sadly, we can't save this foreign key refed object.
+            all_fk_ok = False
+
+    if not all_fk_ok:
+        if raise_on_error:
+            raise ValueError('Model object foreign key object problem.')
+        return False
+    
+    # Now save this!
+    # NOTE: This will fail if default objects are not yet present in the
+    # local default database, because ultimately, those default objects give context
+    # to other objects.
+    ok = default_safe_save_model_object(model_object, raise_on_error=raise_on_error)
     if ok and act_model == AllManifest:
         # Only set the cache if this is a manifest item,
         # which we hit often b/c of foreign key relations
