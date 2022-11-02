22f527c67e5cb73ea41b25d3b21b478576c3f2d9
Eric Kansa <kansaeric@gmail.com>
2022-11-02 15:24:55 -0700

    Deleted deprecated code

--- opencontext_py/apps/edit/inputs/profiles/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-@ensure_csrf_cookie
-def index(request):
-    """nothing to do"""
--- opencontext_py/apps/edit/versioning/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-
-# These views handle requests for interacting with the version history
-def index(request):
-    return HttpResponse("Hello, world. You're at the versioning index.")
-
