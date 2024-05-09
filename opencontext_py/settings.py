cf76e4bad22958871edf256c7e0cfbd548a5d272
Eric Kansa <kansaeric@gmail.com>
2024-05-08 20:50:03 -0700

    Use redis cache for worker

--- opencontext_py/settings.py
+++ opencontext_py/settings.py
@@ -423,13 +423,12 @@ USER_AGENTS_CACHE = 'redis'
 
 RQ_QUEUES = {
     'high': {
-        'URL': os.getenv('REDISTOGO_URL', f'redis://{REDIS_HOST}/0'),
-
-        # 'USE_REDIS_CACHE': 'redis',
+        # 'URL': os.getenv('REDISTOGO_URL', f'redis://{REDIS_HOST}/0'),
+        'USE_REDIS_CACHE': 'redis',
     },
     'low': {
-        'URL': os.getenv('REDISTOGO_URL', f'redis://{REDIS_HOST}/0'),
-        # 'USE_REDIS_CACHE': 'redis',
+        # 'URL': os.getenv('REDISTOGO_URL', f'redis://{REDIS_HOST}/0'),
+        'USE_REDIS_CACHE': 'redis',
     },
 }
 
