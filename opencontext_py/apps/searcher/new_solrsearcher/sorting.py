970c39064da9ac857d3a7bbd44d6b8094c546ca2
Eric Kansa <kansaeric@gmail.com>
2024-09-07 13:56:26 -0700

    Fixed solr sorting

--- opencontext_py/apps/searcher/new_solrsearcher/sorting.py
+++ opencontext_py/apps/searcher/new_solrsearcher/sorting.py
@@ -15,7 +15,7 @@ class SortingOptions():
         self.current_sorting = []
         self.solr_sort_default = configs.SOLR_SORT_DEFAULT
         self.request_sort_dir_delim = configs.REQUEST_SORT_DIR_DELIM
-        self.field_sep = configs.REQUEST_PROP_HIERARCHY_DELIM
+        self.field_sep = configs.REQUEST_SORT_FIELD_DELIM
         self.request_solr_sort_mappings = configs.REQUEST_SOLR_SORT_MAPPINGS
         self.sort_links = []
         self.base_search_url = base_search_url
@@ -79,6 +79,7 @@ class SortingOptions():
 
         # Iterate through a list of sorting arguments.
         for cur_field_raw in self.make_sort_args_list(requested_sort):
+            print(f'sort: {cur_field_raw}')
             order = 'asc'  # the default sort order
             if self.request_sort_dir_delim in cur_field_raw:
                 cur_field_ex = cur_field_raw.split(self.request_sort_dir_delim)
