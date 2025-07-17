88a6d0349d36b53a96b3fdaaceb18a09f37d351d
Eric Kansa <kansaeric@gmail.com>
2025-07-17 15:02:32 -0700

    Initial duckdb project export function

--- opencontext_py/apps/all_items/isamples/utilities.py
+++ opencontext_py/apps/all_items/isamples/utilities.py
@@ -15,14 +15,14 @@ from opencontext_py.apps.all_items.models import (
     AllManifest,
 )
 
-
+from opencontext_py.libs import duckdb_con
 
 
 def cast_duckdb_uuid(uuid):
-    """Convert a manifest object to a DuckDB compatible UUID string"""
-    return f"CAST('{str(uuid)}' AS UUID)"
+    """Convert a uuid to a DuckDB compatible UUID string"""
+    return duckdb_con.cast_duckdb_uuid(uuid)
 
 
 def cast_man_obj_duckdb_uuid(man_obj):
     """Convert a manifest object to a DuckDB compatible UUID string"""
-    return cast_duckdb_uuid(man_obj.uuid)
+    return duckdb_con.cast_duckdb_uuid(man_obj.uuid)
