2f11382734c1ee6195ec3fde606fce1419a96658
Eric Kansa <kansaeric@gmail.com>
2026-03-30 17:24:45 -0700

    Edit to consolidate ARK pre-registration code

--- opencontext_py/apps/persistent_ids/radiocarbon/manage.py
+++ opencontext_py/apps/persistent_ids/radiocarbon/manage.py
@@ -10,12 +10,13 @@ from opencontext_py.apps.persistent_ids.ezid.ezid import EZID, PRE_REGISTER_SHOU
 from opencontext_py.apps.persistent_ids.ezid.metaark import metaARK
 from opencontext_py.apps.persistent_ids.ezid.metadoi import metaDOI
 from opencontext_py.apps.persistent_ids.ezid.manage import EZIDmanage
+from opencontext_py.apps.persistent_ids.preregister import utilities as prereg_utils
 
 from opencontext_py.apps.all_items.models import (
     AllManifest,
     AllIdentifier,
 )
-from opencontext_py.apps.all_items.representations import item
+
 
 """
 Test:
@@ -119,79 +120,6 @@ def create_preregistered_id_from_label(label, id_prefix=ID_PREFIX):
     return f'{id_prefix}{for_ark}'
 
 
-def is_valid_preregistered_id(check_id):
-    """Validates allowed characters and patterns for a pre-registered ID
-
-    :param str check_id: An ARK identifier that we want to check to see if
-        it is a string with valid characters
-
-    returns bool (True if valid, False if not valid)
-    """
-    if not check_id:
-        return False
-    if not isinstance(check_id, str):
-        return False
-    if check_id.startswith('ark:/'):
-        check_id = check_id.split('ark:/')[-1]
-    return re.match(r"^[a-z0-9_]+(?:/[a-z0-9_]+)*\.?[a-z0-9_]+$", check_id) is not None
-
-
-def get_manifest_obj_using_ark_id(check_man_obj, check_id):
-    """Checks to see if the check_id is already in use with a manifest record
-    other than the check_man_obj
-
-    :param AllManifest check_man_obj: The item that we want to check to see
-        if we can use check_id without clashing with other manifest records
-    :param str check_id: An ARK identifier that we want to check to see if
-        it is in use with a manifest record OTHER than check_man_obj
-
-    returns AllManifest that clashes (or None, if no clash)
-    """
-    # Open Context currently doesn't store the 'ark:/' part as part of the
-    # ID in the database. So remove it if present.
-    if check_id.startswith('ark:/'):
-        query_id = check_id.split('ark:/')[-1]
-    else:
-        query_id = check_id
-    # This should return None, if it returns a record, then we've
-    # got a clashing ID problem.
-    clashing_id = AllIdentifier.objects.filter(
-        scheme='ark',
-        id=query_id,
-    ).exclude(
-        item=check_man_obj,
-    ).first()
-    if not clashing_id:
-        # The happy, expected scenario where there is record of this
-        # check_id identifier in use with a different manifest
-        # object.
-        return None
-    # The sad, unexpected scenario where we already have a different
-    # manifest record that has the same check_id
-    return clashing_id.item
-
-
-def get_existing_preregistered_id_obj_for_man_obj(man_obj, id_prefix=ID_PREFIX,):
-    """Gets and existing (if present) pre-registered ID for a Manifest object with a
-    given id_prefix
-
-    :param AllManifest man_obj: The item that may have a preregistered ID.
-    :param str id_prefix: The prefix (scheme, shoulder part, project part)
-
-    returns str a validated, unique pre-registered ID
-    """
-    if id_prefix.startswith('ark:/'):
-        query_id = id_prefix.split('ark:/')[-1]
-    else:
-        query_id = id_prefix
-    id_obj = AllIdentifier.objects.filter(
-        item=man_obj,
-        scheme='ark',
-        id__startswith=query_id,
-    ).first()
-    return id_obj
-
-
 def get_or_make_preregistered_id_for_man_obj(
     man_obj,
     id_prefix=ID_PREFIX,
@@ -205,7 +133,7 @@ def get_or_make_preregistered_id_for_man_obj(
 
     returns str, id_obj (a validated and unique pre-registered ID, existing id object record)
     """
-    id_obj = get_existing_preregistered_id_obj_for_man_obj(
+    id_obj = prereg_utils.get_existing_preregistered_id_obj_for_man_obj(
         man_obj,
         id_prefix=id_prefix,
     )
@@ -218,11 +146,11 @@ def get_or_make_preregistered_id_for_man_obj(
         label=man_obj.label,
         id_prefix=id_prefix,
     )
-    if not is_valid_preregistered_id(preregistered_id):
+    if not prereg_utils.is_valid_preregistered_id(preregistered_id):
         # We failed to make a valid pre-registered ID
         raise ValueError(f'{man_obj.label} [{str(man_obj.uuid)}] led to invalid ark: {preregistered_id}')
     # Now check to see if we have a clashing use of the preregistered_id
-    clashing_obj = get_manifest_obj_using_ark_id(man_obj, preregistered_id)
+    clashing_obj = prereg_utils.get_manifest_obj_using_ark_id(man_obj, preregistered_id)
     if clashing_obj:
         if non_unique_pid_handling == 'db_store':
             man_obj.meta_json['clashing_pid'] = preregistered_id
